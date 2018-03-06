22f6190320d75af79b76e633ef9ef740a6b9aa48
Charlie <36271978+CharlieV2@users.noreply.github.com>
2018-03-06 11:29:48 +0300

    Add files via upload

---
+++ getWeather.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = Request('https://www.wunderground.com/history/airport/UUEE/2018/2/1/DailyHistory.html', headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(url).read()
+soup = BeautifulSoup(webpage)
+values = soup.findAll(attrs={"class":"wx-value"})
+print(values[4])
