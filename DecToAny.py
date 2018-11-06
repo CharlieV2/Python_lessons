ee927699ad30c31cbf0c932bc8c53bfa6cbbe77a
Charlie <36271978+CharlieV2@users.noreply.github.com>
2018-11-06 23:54:48 +0300

    Update DecToAny.py

--- DecToAny.py
+++ DecToAny.py
@@ -1,13 +1,17 @@
 InputNumber = int(input('Число в десятичной СС: '))
 NumSys = int(input('Перевести в СС: '))
 
-List = ["A", "B", "C", "D", "E", "F", "G", ]
+List = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ]
 ResultNumber = ''
 
 while InputNumber >= NumSys:
     Temp = InputNumber % NumSys
     if (Temp >= 10):
-        Temp = List[(Temp%10)]
+        if (Temp >= 20):
+            Temp = List[(Temp%10) + 10]
+        else:
+            Temp = List[(Temp%10)]
+        
         
     ResultNumber = str(Temp) + ResultNumber
     InputNumber = InputNumber // NumSys
